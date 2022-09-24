dbe63e15149d2134998c18cc6f0aec3ed62d2f71
SimplisticCode <42718252+SimplisticCode@users.noreply.github.com>
2022-09-23 17:55:45 -0700

    Updated BlueSpecParser

--- src/main/scala/DocumentEnrichers/BlueSpecParser.scala
+++ src/main/scala/DocumentEnrichers/BlueSpecParser.scala
@@ -6,7 +6,12 @@ abstract sealed class AnyArgument
 
 case class IdRef(str: String) extends AnyArgument
 
+case class NumberIdRef(int: Int) extends AnyArgument
+
+// Symbols
 case class DOT()
+
+case class COMMA()
 
 case class LPAREN()
 
@@ -26,6 +31,10 @@ case class RBRACE()
 
 case class SEMICOLON()
 
+case class HASHTAG()
+
+// Keywords
+
 case class RULE()
 
 case class MODULE()
@@ -44,86 +53,167 @@ case class FUNCTION()
 
 case class DIRECTION()
 
+case class STRUCT()
+
+case class ENUM()
+
+case class DERIVING()
+
+
+// Types
+case class BOOLEAN()
+
+case class BIT()
+
+case class VECTOR()
+
+case class ACTION()
+
+case class ACTIONVALUE()
 
 
 trait IdentifierParser extends RegexParsers {
-  def identifier: Parser[IdRef] = """\W+""".r ^^ { str => IdRef(str) }
+  def identifier: Parser[IdRef] = """\w+""".r ^^ { str => IdRef(str) }
+
+  def numberIdentifier: Parser[NumberIdRef] = """\d+""".r ^^ { int => NumberIdRef(int.toInt) }
+
 }
 
-class BlueSpecParser extends IdentifierParser {
-  def dot: Parser[DOT] = "\\.".r ^^ { _ => DOT() }
+trait TypeParser extends RegexParsers {
+  def boolean: Parser[BOOLEAN] = "Bool" ^^ { _ => BOOLEAN() }
 
-  def lparen: Parser[LPAREN] = "\\(".r ^^ { _ => LPAREN() }
+  def bit: Parser[BIT] = "Bit" ^^ { _ => BIT() }
 
-  def rparen: Parser[RPAREN] = "\\)".r ^^ { _ => RPAREN() }
+  def vector: Parser[VECTOR] = "Vector" ^^ { _ => VECTOR() }
 
-  def lbrace: Parser[LBRACE] = "\\{".r ^^ { _ => LBRACE() }
+  def action: Parser[ACTION] = "Action" ^^ { _ => ACTION() }
 
-  def rbrace: Parser[RBRACE] = "\\}".r ^^ { _ => RBRACE() }
+  def actionValue: Parser[ACTIONVALUE] = "ActionValue" ^^ { _ => ACTIONVALUE() }
 
-  def color: Parser[COLON] = ":".r ^^ { _ => COLON() }
 
-  def astrix: Parser[ASTRIX] = "\\*".r ^^ { _ => ASTRIX() }
+}
 
-  def lbracket: Parser[LBRACKET] = "\\[".r ^^ { _ => LBRACKET() }
+trait SymbolParser extends RegexParsers {
+  def dot: Parser[DOT] = "." ^^ { _ => DOT() }
 
-  def rbracket: Parser[RBRACKET] = "\\]".r ^^ { _ => RBRACKET() }
+  def comma: Parser[COMMA] = "," ^^ { _ => COMMA() }
 
-  def semicolon: Parser[SEMICOLON] = ";".r ^^ { _ => SEMICOLON() }
+  def lparen: Parser[LPAREN] = "(" ^^ { _ => LPAREN() }
 
-  def rule: Parser[RULE] = "rule".r ^^ { _ => RULE() }
+  def rparen: Parser[RPAREN] = ")" ^^ { _ => RPAREN() }
 
-  def module: Parser[MODULE] = "module".r ^^ { _ => MODULE() }
+  def lbracket: Parser[LBRACKET] = "[" ^^ { _ => LBRACKET() }
 
-  def blueSpecImport: Parser[IMPORT] = "import".r ^^ { _ => IMPORT() }
+  def rbracket: Parser[RBRACKET] = "]" ^^ { _ => RBRACKET() }
 
-  def interface: Parser[INTERFACE] = "interface".r ^^ { _ => INTERFACE() }
+  def colon: Parser[COLON] = ":" ^^ { _ => COLON() }
 
-  def method: Parser[METHOD] = "method".r ^^ { _ => METHOD() }
+  def astrix: Parser[ASTRIX] = "*" ^^ { _ => ASTRIX() }
 
-  def blueSpecTypeDef: Parser[TYPEDEF] = "typedef".r ^^ { _ => TYPEDEF() }
+  def lbrace: Parser[LBRACE] = "{" ^^ { _ => LBRACE() }
 
-  def blueSpecPackage: Parser[PACKAGE] = "package".r ^^ { _ => PACKAGE() }
+  def rbrace: Parser[RBRACE] = "}" ^^ { _ => RBRACE() }
+
+  def semicolon: Parser[SEMICOLON] = ";" ^^ { _ => SEMICOLON() }
+
+  def hashtag: Parser[HASHTAG] = "#" ^^ { _ => HASHTAG() }
+}
+
+trait KeywordParser extends RegexParsers {
+  def rule: Parser[RULE] = "rule" ^^ { _ => RULE() }
+
+  def module: Parser[MODULE] = "module" ^^ { _ => MODULE() }
+
+  def importKeyword: Parser[IMPORT] = "import" ^^ { _ => IMPORT() }
+
+  def interface: Parser[INTERFACE] = "interface" ^^ { _ => INTERFACE() }
+
+  def method: Parser[METHOD] = "method" ^^ { _ => METHOD() }
+
+  def typedef: Parser[TYPEDEF] = "typedef" ^^ { _ => TYPEDEF() }
+
+  def stuctKeyword: Parser[STRUCT] = "struct" ^^ { _ => STRUCT() }
+
+  def enumKeyword: Parser[ENUM] = "enum" ^^ { _ => ENUM() }
+
+  def packageKeyword: Parser[PACKAGE] = "package" ^^ { _ => PACKAGE() }
+
+  def function: Parser[FUNCTION] = "function" ^^ { _ => FUNCTION() }
 
   def direction: Parser[DIRECTION] = ("in".r | "out".r) ^^ { str => DIRECTION() }
 
-  def function: Parser[FUNCTION] = "function".r ^^ { _ => FUNCTION() }
+  def derivingKeyword: Parser[DERIVING] = "deriving" ^^ { _ => DERIVING() }
 
-  def methodParsing: Parser[BlueSpecMethod] = method ~ direction ~ identifier ^^ {
-    case _ ~ _ ~ IdRef(methodName) => BlueSpecMethod(methodName)
+}
+
+class BlueSpecParser extends IdentifierParser
+  with TypeParser
+  with SymbolParser
+  with KeywordParser {
+
+  def methodParsing: Parser[BlueSpecMethod] = method ~ opt(typeParser) ~ identifier ~ opt(lparen ~ rep(argumentParser) ~ rparen) ^^ {
+    case _ ~ _ ~ IdRef(methodName) ~ Some(_ ~ arguments ~ _) => BlueSpecMethod(methodName, None, arguments)
+    case _ ~ None ~ IdRef(methodName) ~ Some(_ ~ arguments ~ _) => BlueSpecMethod(methodName, None, arguments)
+    case _ ~ _ ~ IdRef(methodName) ~ None => BlueSpecMethod(methodName, None, List())
+    case _ ~ None ~ IdRef(methodName) ~ None => BlueSpecMethod(methodName, None, List())
   }
 
-  def packageParsing: Parser[BlueSpecPackage] = blueSpecPackage ~ identifier ~ semicolon ^^ {
-    case _ ~ IdRef(packageName) => BlueSpecPackage(packageName)
+  def packageParsing: Parser[BlueSpecPackage] = packageKeyword ~ identifier ~ semicolon ^^ {
+    case _ ~ IdRef(packageName) ~ _ => BlueSpecPackage(packageName)
   }
 
-  def typeParsing: Parser[BlueSpecType] = blueSpecTypeDef ~ lbrace ~ identifier ~ rbrace ~ identifier ~ semicolon ^^ {
-    case _ ~ _ ~ _ ~ _ ~ IdRef(typeName) => BlueSpecType(typeName)
+  def typeStructParsing: Parser[BlueSpecStructType] = typedef ~ stuctKeyword ~ lbrace ~ rep(argumentParser) ~ rbrace ~ identifier ~ semicolon ^^ {
+    case _ ~ _ ~ _ ~ _ ~ _ ~ IdRef(typeName) ~ _ => BlueSpecStructType(typeName)
+  }
+
+  def derivingParsing: Parser[String] = derivingKeyword ~ lparen ~ identifier ~ rep(comma ~ identifier) ~ rparen ^^ {
+    case _ ~ _ ~ IdRef(derivingName) ~ _ ~ _ => derivingName
+  }
+
+  def identifierList: Parser[List[IdRef]] = rep1sep(identifier, comma)
+
+  def typeEnumsParsing: Parser[BlueSpecEnumType] = typedef ~ enumKeyword ~ lbrace ~ identifierList ~ rbrace ~ identifier ~ opt(derivingParsing) ~ semicolon ^^ {
+    case _ ~ _ ~ _ ~ enumList ~ _ ~ IdRef(typeName) ~ derivingName ~ _ => BlueSpecEnumType(typeName)
   }
 
   def moduleParsing: Parser[BlueSpecModule] = module ~ identifier ~ lparen ~ identifier ~ rparen ~ semicolon ^^ {
     case _ ~ IdRef(moduleName) ~ _ ~ IdRef(argument) ~ _ ~ _ => BlueSpecModule(moduleName, argument)
   }
 
-  def interfaceParsing: Parser[BlueSpecInterface] = interface ~ identifier ~ COLON ~ COLON ~ ASTERIX ~ semicolon ^^ {
-    case _ ~ IdRef(interfaceName) ~ _ ~ _ ~ _ ~ _ => BlueSpecInterface(interfaceName)
+  def interfaceParsing: Parser[BlueSpecInterface] = interface ~ identifier ~ opt(identifier) ~ semicolon ^^ {
+    case _ ~ IdRef(interfaceName) ~ name ~ _ =>
+      name match {
+        case Some(IdRef(name)) => BlueSpecInterface(interfaceName, Some(name))
+        case None => BlueSpecInterface(interfaceName)
+      }
   }
 
   def ruleParsing: Parser[BlueSpecRule] = rule ~ identifier ~ semicolon ^^ {
     case _ ~ IdRef(interfaceName) ~ _ => BlueSpecRule(interfaceName)
   }
 
-  def blueSpecImportParsing: Parser[BlueSpecImport] = blueSpecImport ~ identifier ~ semicolon ^^ {
-    case _ ~ IdRef(importName) ~ _ => BlueSpecImport(importName)
+  def blueSpecImportParsing: Parser[BlueSpecImport] = importKeyword ~ identifier ~ repN(2, colon) ~ astrix ~ semicolon ^^ {
+    case _ ~ IdRef(importName) ~ _ ~ _ ~ _ => BlueSpecImport(importName)
   }
 
+  def uniType: Parser[Product] = boolean | action
+
+  def composableTypes: Parser[Product] = vector | actionValue | bit
+
+  def typeParser: Parser[Product] = uniType | composableTypes
+
+  def argumentParser: Parser[String] = typeParser ~ identifier ^^ {
+    case _ ~ IdRef(argumentName) => argumentName
+  }
 }
+
+object BlueSpecParserSingleton extends BlueSpecParser
 
 final case class BlueSpecImport(importName: String) {
   require(importName.nonEmpty, "importName must not be empty")
 }
 
-final case class BlueSpecMethod(name: String) {
+final case class BlueSpecMethod(name: String, returnType: Option[String], parameters: List[String]) {
   require(name.nonEmpty, "Method name cannot be empty")
 }
 
@@ -133,7 +223,7 @@ final case class BlueSpecModule(name: String, argument: String) {
   require(argument.nonEmpty, "Module argument cannot be empty")
 }
 
-final case class BlueSpecInterface(name: String) {
+final case class BlueSpecInterface(name: String, argument: Option[String] = None) {
   require(name.nonEmpty, "Interface name cannot be empty")
 }
 
@@ -145,7 +235,12 @@ final case class BlueSpecPackage(name: String) {
   require(name.nonEmpty, "Package name cannot be empty")
 }
 
-final case class BlueSpecType(name: String) {
+final case class BlueSpecEnumType(name: String) {
   require(name.nonEmpty, "Type name cannot be empty")
 }
+
+final case class BlueSpecStructType(name: String) {
+  require(name.nonEmpty, "Type name cannot be empty")
+}
+
 
---
+++ src/test/scala/DocumentEnrichers/BlueSpecParserSingletonTest.scala
@@ -0,0 +1,153 @@
+package DocumentEnrichers
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class BlueSpecParserSingletonTest extends AnyFlatSpec with should.Matchers {
+
+  it should "be able to parse import statements" in {
+    val input = "import ruleName::*;"
+    val input2 = "import Nerv_BVI :: *;"
+
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.blueSpecImportParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.importName should be("ruleName")
+
+    val parsedResult2 = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.blueSpecImportParsing, input2)
+    parsedResult2.successful should be(true)
+
+    val result2 = parsedResult2.get
+    result2.importName should be("Nerv_BVI")
+  }
+
+  it should "be able to parse module statements" in {
+    val input = "module mkInstrGeneratedIsChannelTripped (ChannelTripped_IFC);"
+
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.moduleParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("mkInstrGeneratedIsChannelTripped")
+    result.argument should be("ChannelTripped_IFC")
+
+    val input2 = "module mkActuationGenerated(Actuation_IFC);" // no space between module name and opening parenthesis
+    val parsedResult2 = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.moduleParsing, input2)
+    parsedResult2.successful should be(true)
+
+    val result2 = parsedResult2.get
+    result2.name should be("mkActuationGenerated")
+    result2.argument should be("Actuation_IFC")
+  }
+
+  it should "be able to parse typeDef struct " in {
+    val input = "typedef struct { Bit #(4) name1; Bit #(32) name2; } DmemWrite deriving (Bits, Eq, FShow);"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.typeStructParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("DmemWrite")
+  }
+
+  it should "be able to parse typeDef enum " in {
+    val input = "typedef enum { REQ_I, PUSH_I, REQ_D, PUSH_D } DmemWrite;"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.typeEnumsParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("DmemWrite")
+  }
+
+  it should "be able to parse typeDef enum deriving something " in {
+    val input = "typedef enum { REQ_I, PUSH_I, REQ_D, PUSH_D } DmemWrite deriving(Bits, Eq, FShow);"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.typeEnumsParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("DmemWrite")
+  }
+
+  it should "be able to parse an interface definition" in {
+    val input = "interface ChannelTripped_IFC;"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.interfaceParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("ChannelTripped_IFC")
+  }
+
+  it should "be able to parse an interface definition with name" in {
+    val input = "interface ChannelTripped_IFC name00;"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.interfaceParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("ChannelTripped_IFC")
+    result.argument should be(Some("name00"))
+  }
+
+  it should "be able to parse a rule with no parameters" in {
+    val input = "rule ruleName;"
+
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.ruleParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("ruleName")
+  }
+
+  it should "be able to parse a rule with parameters" in {
+    val input = "rule ruleName;"
+
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.ruleParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("ruleName")
+  }
+
+  it should "be able to parse method with no parameters" in {
+    val input = "method method1;"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.methodParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("method1")
+    result.returnType should be(None)
+    result.parameters should be(Nil)
+  }
+
+  it should "be able to parse method with parameters" in {
+    val input = "method method1(Bool b);"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.methodParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("method1")
+    result.returnType should be(None)
+  }
+
+
+  it should "be able to parse method with return type" in {
+    val input = "method Action method1;"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.methodParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("method1")
+    result.returnType should be(Some("Action"))
+  }
+
+  it should "be able to parse method with return type and parameters" in {
+    val input = "method Action method1 (Bool b);"
+    val parsedResult = BlueSpecParserSingleton.parse(BlueSpecParserSingleton.methodParsing, input)
+    parsedResult.successful should be(true)
+
+    val result = parsedResult.get
+    result.name should be("method1")
+    result.returnType should be(Some("Action"))
+    result.parameters should be(Some("Bool b"))
+  }
+
+}
